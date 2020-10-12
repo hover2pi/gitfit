ef90e4d592c0166a26054c81b74904b966d36054
Joe Filippazzo <jfilippazzo@stsci.edu>
2020-10-12 10:32:05 -0400

    Fixed importing and updated to v0.1.1

--- __init__.py
+++ __init__.py
@@ -1,1 +1,9 @@
-from gitfit import *
+# -*- coding: utf-8 -*-
+
+"""Top-level dir for gitfit."""
+
+__author__ = """Joe Filippazzo"""
+__email__ = 'jfilippazzo@stsci.edu'
+__version__ = '0.1.1'
+
+from .gitfit import reassemble, disassemble, make_dummy_file
